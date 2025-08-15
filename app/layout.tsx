8a28ea90cfe45c338fa0fe4a424ff474644d34d9
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-08-15 20:21:32 +0100

    feat: implement authentication with NextAuth and Prisma integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Session_Provider from "@/providers/session-providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Session_Provider>
+
+            {children}
+        </Session_Provider>
       </body>
     </html>
   );
---
+++ utils/authOptions.ts
@@ -0,0 +1,59 @@
+import  { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+
+import { Prisma } from "@/prisma/prisma";
+import { Adapter } from "next-auth/adapters";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+declare module "next-auth" {
+    interface Session {
+      user: {
+        id: string;
+        name: string;
+        email: string;
+        image?: string;
+      };
+    }
+  
+    interface JWT {
+      id: string;
+    }
+  }
+  
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(Prisma) as unknown as Adapter,
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if(session.user){
+
+                session.user.id = token.id as string;
+            }
+            return session;
+        },
+    },
+    secret: process.env.NEXT_PUBLIC_SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    debug: process.env.NODE_ENV === "development",
+};
